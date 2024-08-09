6bd0d93d5326eda53ef1abc82adadd56ec7be700
eldahl <71466904+eldahl@users.noreply.github.com>
2024-08-09 15:37:45 +0200

    Initial dotfiles upload

---
+++ .config/screenlayout/config.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --primary --mode 1920x1200 --pos 0x528 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 3840x0 --rotate left --output HDMI-1 --off --output DP-0 --mode 1920x1200 --pos 1920x528 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off
