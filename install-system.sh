05287de968170c48c77991da0b1ba9d42b4dbd43
eldahl <71466904+eldahl@users.noreply.github.com>
2024-08-13 21:56:51 +0200

    added cmake and ninja

--- install-system.sh
+++ install-system.sh
@@ -2,7 +2,7 @@
 
 # Install script for applications and tools assuming base Arch Linux
 # system with packages: base linux linux-firmware neovim amd-ucode efibootmgr grub
-sudo pacman --needed -S alsa-firmware alsa-utils arandr bitwarden clang discord dmenu dolphin fastfetch feh firefox font-manager git github-cli gnome-keyring htop i3-wm i3status kitty less noto-fonts-emoji nvidia nerd-fonts pasystray picom pipewire pipewire-alsa pipewire-pulse polkit-kde-agent sof-firmware starship sudo thunderbird ufw which xorg xorg-xinit
+sudo pacman --needed -S alsa-firmware alsa-utils arandr bitwarden clang cmake ninja discord dmenu dolphin fastfetch feh firefox font-manager git github-cli gnome-keyring htop i3-wm i3status kitty less noto-fonts-emoji nvidia nerd-fonts pasystray picom pipewire pipewire-alsa pipewire-pulse polkit-kde-agent sof-firmware starship sudo thunderbird ufw which xorg xorg-xinit
 sudo pacman --needed -S rawtherapee gimp obsidian gvfs gphoto2 xdg-desktop-portal syncthing spectacle raw-thumbnailer kdesdk-thumbnailers kdegraphics-thumbnailers i3lock gwenview xclip freecad
 
 # Install yay for AUR packages
