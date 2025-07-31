4bf2284880b3f4c69a94dca745a21ff3580e6766
eldahl <71466904+eldahl@users.noreply.github.com>
2025-07-31 20:41:44 +0200

    long due update

--- .bashrc
+++ .bashrc
@@ -1,3 +1,22 @@
 eval "$(starship init bash)"
 export PATH="/home/eldahl/bin:$PATH"
 export PATH="$PATH:$HOME/.dotnet/tools/"
+export PATH="$PATH:/opt/anaconda/bin/"
+
+export CRYPTOGRAPHY_OPENSSL_NO_LEGACY=true
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+#__conda_setup="$('/opt/anaconda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+#if [ $? -eq 0 ]; then
+#    eval "$__conda_setup"
+#else
+#    if [ -f "/opt/anaconda/etc/profile.d/conda.sh" ]; then
+#        . "/opt/anaconda/etc/profile.d/conda.sh"
+#    else
+#        export PATH="/opt/anaconda/bin:$PATH"
+#    fi
+#fi
+#unset __conda_setup
+# <<< conda initialize <<<
+
--- .config/screenlayout/config.sh
+++ .config/screenlayout/config.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --primary --mode 1920x1200 --pos 0x521 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 3840x0 --rotate left --output HDMI-1 --off --output DP-0 --mode 1920x1200 --pos 1920x521 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DVI-D-2-0 --off --output HDMI-2-0 --off --output HDMI-2-1 --off --output DP-2-0 --off --output DP-2-1 --off --output DP-2-2 --off --output DP-2-3 --off
+xrandr --output HDMI-0 --mode 1920x1080 --pos 3840x0 --rotate left --output DP-0 --mode 1920x1200 --pos 1920x609 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 1920x1200 --pos 0x609 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off
